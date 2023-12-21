5c8db3e708c191d032f1b86d42b6a90465740553
Heo Jisoo <barley6m@gmail.com>
2023-12-21 15:41:37 +0900

    Merge branch 'master' of github.com:codingbori/studyProject1
    # Conflicts:
    #	package.json

--- src/components/Post.js
+++ src/components/Post.js
@@ -1,30 +1,37 @@
 import Comments from "./Comments";
 import "./Post.css";
 import posts from "../assets/datas/postData";
+import { type } from "os";
 const Post = () => {
   const postId = Number(localStorage.getItem("postingNow"));
-  let p = null;
-  for (const post of posts) {
-    if (postId === post.id) {
-      p = post;
-      break;
+
+  async function logJSONData() {
+    try {
+      const response = await fetch("http://localhost:3000/posts");
+      const jsonData = await response.json();
+      console.log("type: " + typeof jsonData);
+      console.log(jsonData);
+    } catch (err) {
+      console.log(err);
     }
   }
 
-  const postImages = [];
-  if (p.imageUrl) {
-    p.imageUrl.map((obj, index) => {
-      postImages.push(<img src={obj.src} alt={obj.alt} key={index} />);
-    });
-  }
+  logJSONData();
+
+  // const postImages = [];
+  // if (p.imageUrl) {
+  //   p.imageUrl.forEach((obj, index) => {
+  //     postImages.push(<img src={obj.src} alt={obj.alt} key={index} />);
+  //   });
+  // }
 
   return (
     <>
       <article className="post">
-        <h2 className="post-title">{p.title}</h2>
-        <p className="post-author">{p.userid}</p>
-        <p className="post-text">{p.text}</p>
-        <div className="post-image">{postImages}</div>
+        <h2 className="post-title">p.title</h2>
+        <p className="post-author">p.userid</p>
+        <p className="post-text">p.text</p>
+        {/* <div className="post-image">{postImages}</div> */}
       </article>
       <Comments />
     </>
