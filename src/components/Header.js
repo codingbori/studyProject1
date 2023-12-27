a53930e5f8e38b269a8ea8f440207a94c1e2bec4
Heo Jisoo <barley6m@gmail.com>
2023-12-28 06:52:21 +0900

    8차

--- src/components/Header.js
+++ src/components/Header.js
@@ -7,7 +7,34 @@ const Header = (props) => {
   //카카오 로그인에 미친 사람
   const getToken = useLocation().search;
   let code = new URLSearchParams(getToken).get("code") || null;
-  if (code) {
+
+  displayToken();
+  function displayToken() {
+    var token = getCookie("authorize-access-token");
+
+    if (token) {
+      window.Kakao.Auth.setAccessToken(token);
+      window.Kakao.Auth.getStatusInfo()
+        .then(function (res) {
+          if (res.status === "connected") {
+            document.getElementById("token-result").innerText =
+              "login success, token: " + window.Kakao.Auth.getAccessToken();
+          }
+        })
+        .catch(function (err) {
+          window.Kakao.Auth.setAccessToken(null);
+        });
+    }
+  }
+
+  function getCookie(name) {
+    var parts = document.cookie.split(name + "=");
+    if (parts.length === 2) {
+      return parts[1].split(";")[0];
+    }
+  }
+
+  if (false) {
     //토큰을 받아요~~
     console.log("코드: ", code);
     fetch("https://kauth.kakao.com/oauth/token", {
